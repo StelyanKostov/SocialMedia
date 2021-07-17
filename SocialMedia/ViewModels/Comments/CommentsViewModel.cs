e7ac83bea101e820c5456c39bd426d60399b19d7
StelyanKostov <kostovstelyan@gmail.com>
2021-07-17 11:31:25 +0300

    pop up chat

--- SocialMedia/ViewModels/Comments/CommentsViewModel.cs
+++ SocialMedia/ViewModels/Comments/CommentsViewModel.cs
@@ -1,8 +1,5 @@
 ﻿using SocialMedia.ViewModels.Image;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SocialMedia.ViewModels.Comments
 {
